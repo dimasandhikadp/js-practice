8cea268148b75d49eb085ac4bae19cafb1e9c6bc
dimasandhikadp <dimasandhikadwiperman@gmail.com>
2026-04-02 20:19:13 +0800

    feat: implement add task feature

---
+++ task-tracker/script.js
@@ -0,0 +1,52 @@
+//JSON initial section
+const fs = require("fs");
+const filePath = "tasks.json";
+
+//Cek apakah file json ada
+if (!fs.existsSync(filePath)) {
+  fs.writeFileSync(filePath, "[]");
+}
+
+//Baca isi file JSON
+const data = fs.readFileSync(filePath, "utf-8");
+
+//Ubah isi file JSON ke Array
+const tasks = JSON.parse(data);
+
+//Command initial section
+const command = process.argv[2];
+const value = process.argv[3];
+
+switch (command) {
+  case "add":
+    console.log("Menambahkan task...");
+
+    const now = new Date().toISOString();
+    const newId = tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1;
+    const newTask = {
+      id: newId,
+      description: value,
+      status: "todo",
+      createdAt: now,
+      updatedAt: now
+    };
+
+    tasks.push(newTask);
+    fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+
+    console.log("Task Berhasil ditambah ID:", newId);
+    break;
+  case "list":
+    console.log("Menampilkan task...");
+    console.log(tasks);
+    break;
+  case "update":
+    console.log("Memperbarui task...");
+    break;
+  case "delete":
+    console.log("Menghapus task...");
+    break;
+  default:
+    console.log("Perintah tidak valid...");
+    break;
+}
