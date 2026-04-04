50f7afdd0a3036fad97f1ecc3aecae3ea82ff4a8
dimasandhikadp <dimasandhikadwiperman@gmail.com>
2026-04-04 21:15:07 +0800

    feat: implement task stastus command

--- task-tracker/script.js
+++ task-tracker/script.js
@@ -76,7 +76,37 @@ switch (command) {
       writeFileSync(filePath, JSON.stringify(filteredTask, null, 2));
       console.log(`Task ID ${deleteId} berhasil dihapus.`)
     }
+    break;
+  case "mark-in-progress":
+    console.log("Mengubah task menjadi in-progress...");
+    
+    const progressId = Number(value);
+    const taskInProgress = tasks.find((task) => task.id === progressId);
 
+    if (!taskInProgress) {
+      console.log(`Task dengan ID ${progressId} tidak ditemukan.`)
+    }else{
+      taskInProgress.status = "in-progress";
+      taskInProgress.updatedAt = new Date().toISOString();
+
+      writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+      console.log(`Task ID ${progressId} berhasil diubah menjadi in-progress`)
+    }
+    break;
+  case "mark-done":
+    console.log("Mengubah task menjadi done...");
+
+    const doneId = Number(value);
+    const taskDone = tasks.find((task) => task.id === doneId);
+
+    if (!taskDone) {
+      console.log(`Task dengan ID ${doneId} tidak ditemukan.`)
+    }else{
+      taskDone.status = "done";
+      taskDone.updatedAt = new Date().toISOString();
+      writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+      console.log(`Task ID ${doneId} berhasil diubah menjadi done.`)
+    }
     break;
   default:
     console.log("Perintah tidak valid...");
